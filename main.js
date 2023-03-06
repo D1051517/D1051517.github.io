1a7f4d4fa2fbf696de2acfcf9ec3e8d42c290069
D1051517 <xdxd15915@gmail.com>
2023-03-06 21:32:39 +0800

    'index.html'

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var countDownElement = document.getElementById("countdown");
 function updateCountDown(){
     countDownElement.innerHTML =   countDownSeconds + "秒後重新整理";
     countDownSeconds--;
+    if(countDownSeconds < 0){
+        countDownElement.innerHTML =   0 + "秒後重新整理";
+    }
 }
 
 var countDownTimer = setInterval(updateCountDown, 1000);
