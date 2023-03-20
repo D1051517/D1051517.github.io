0df8f18bf1d856a4462ea22c896ed484bad0385d
D1051517 <xdxd15915@gmail.com>
2023-03-20 20:20:03 +0800

    end update

--- version2/main.js
+++ version2/main.js
@@ -3,10 +3,10 @@ var countDownSeconds = 5;
 var countDownElement = document.getElementById("countdown");
 
 function updateCountDown(){
-    countDownElement.innerHTML =   countDownSeconds + "秒後重新整理";
+    countDownElement.innerHTML = countDownSeconds + "秒後重新整理";
     countDownSeconds--;
     if(countDownSeconds < 0){
-        countDownElement.innerHTML =   0 + "秒後重新整理";
+        countDownElement.innerHTML = 0 + "秒後重新整理";
     }
 }
 
